3a19c7e94eec3ea2947670b6b3cfb6b813610774
pbabrovich <pl.babrowicz@gmail.com>
2025-05-02 12:46:00 +0200

    Use constants in test

--- src/test/java/task/scoreboard/ScoreBoardTest.java
+++ src/test/java/task/scoreboard/ScoreBoardTest.java
@@ -5,15 +5,17 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ScoreBoardTest {
+    private static final String MEXICO = "Mexico";
+    private static final String CANADA = "Canada";
 
     @Test
     void shouldStartMatchSuccessfully() {
         ScoreBoard scoreboard = new ScoreBoard();
-        scoreboard.startMatch("Mexico", "Canada");
+        scoreboard.startMatch(MEXICO, CANADA);
 
         assertEquals(1, scoreboard.getSummary().size());
-        assertEquals("Mexico", scoreboard.getSummary().get(0).getHomeTeam());
-        assertEquals("Canada", scoreboard.getSummary().get(0).getAwayTeam());
+        assertEquals(MEXICO, scoreboard.getSummary().get(0).getHomeTeam());
+        assertEquals(CANADA, scoreboard.getSummary().get(0).getAwayTeam());
     }
 }
 
